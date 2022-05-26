dbe404702b8b6251b055a8254a610b1bb5df0694
0utrunner <105307161+0utrunner@users.noreply.github.com>
2022-05-25 22:39:32 -0500

    Python Solved

--- python/factorial.py
+++ python/factorial.py
@@ -1,2 +1,10 @@
+import functools
+import operator
+
 def factorial(num):
-	# your code here
+	numbers = []
+	for i in range(num):
+		numbers.append(i + 1)
+		
+	return(functools.reduce(operator.mul, numbers))
+	
